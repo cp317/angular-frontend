052cd2fd52022dda6f0525dc10acefcc6dea872c
Richard Hugessen <richard@hugessen.ca>
2017-07-14 17:33:49 -0400

    Added Google Maps API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  NgModule
+} from '@angular/core';
+
+import {
+  BrowserModule
+} from '@angular/platform-browser';
+
+import { AgmCoreModule } from '@agm/core';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .sebm-google-map-container {
+      height: 400px;
+      width: 100%;
+      float: right;
+     }
+  `],
+  templateUrl:'app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  // google maps zoom level
+  zoom: number = 13;
+
+  // initial center position for the map
+  lat: number = 43.4724;
+  lng: number = -80.5263;
+
+  clickedBeacon(label: string, index: number) {
+    console.log(`clicked the beacon: ${label || index}`)
+  }
+
+  // mapClicked($event: MouseEvent) {
+  //   this.beacons.push({
+  //     lat: $event.coords.lat,
+  //     lng: $event.coords.lng,
+  //     course: "CP317",
+  //     draggable: true
+  //   });
+  // }
+
+  beaconDragEnd(b: beacon, $event: MouseEvent) {
+    console.log('dragEnd', b, $event);
+  }
+
+  beacons: beacon[] = [
+	  {
+		  lat: 43.4724,
+		  lng: -80.526,
+		  course: "NULL",
+		  draggable: false
+	  },
+	  {
+		  lat: 43.4750,
+		  lng: -80.526,
+		  course: "NULL",
+		  draggable: false
+	  },
+  ]
 }
+// just an interface for type safety.
+interface beacon {
+	lat: number;
+	lng: number;
+	id?: string;
+	course: string;
+	draggable: boolean;
+}
+
+@NgModule({
+  imports: [ BrowserModule, AgmCoreModule.forRoot() ],
+  declarations: [ Map ],
+  bootstrap: [ Map ]
+})
+export class MapModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
+import { BeaconComponent } from './beacon/beacon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeaconComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey:'AIzaSyC6ySrInpH3svhfDbZDIWc3dhHAOvZW2kk'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
