5ab245ed30fbba404a8ee161becddec57349292a
Jake-L <ekaj@sympatico.ca>
2017-07-19 19:23:51 -0400

    added beacon constructor and more user attributes

--- src/app/beacon.ts
+++ src/app/beacon.ts
@@ -5,12 +5,10 @@ export class Beacon {
   lat:number;
   lng:number;
   members:string[];
+	school:string;
   startTime:number;
   tags:string;
 
-  getLat(){
-    return this.lat;
-  }
   constructor(b:any){
     this.course = b.course;
     this.endTime = b.endTime;
@@ -20,5 +18,35 @@ export class Beacon {
     this.members = b.members;
     this.startTime = b.startTime;
     this.tags = b.tags;
+  }
+	
+	constructor(school:string, course:string, host:string, startTime:number, endTime:number, tags:string, lat:number, lng:number)
+	{
+		// set the given attributes to the beacon class instance
+		this.course = course;
+    this.endTime = endTime;
+    this.host = host;
+    this.lat = lat;
+    this.lng = lng;
+    this.members = members;
+    this.startTime = startTime;
+    this.tags = tags;
+		
+		// insert the database object into the database
+		database.ref('/beacon/').push({
+			school: this.school,
+			course: this.course,
+			startTime: this.startTime,
+			endTime: this.endTime,
+			host: this.host,
+			tags: this.tags,
+			members: this.members,
+			lat: this.lat,
+			lng: this.lng
+		});
+	}
+	
+  getLat(){
+    return this.lat;
   }
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,31 +1,50 @@
-export class User {
-  constructor(name){
+export class User 
+{
+	this.firstName:string;
+	this.lastName:string;
+	this.email:string;
+	this.beacons:Beacon[];
+
+  constructor(firstName:string, lastName:string, email:string)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+  }
+  joinBeacon()
+	{
 
   }
-  joinBeacon(){
-
-  }
-  leaveBeacon(){
+  leaveBeacon()
+	{
 
   }
 }
 
 export class RegisteredUser {
-  user:User;
-  constructor(user:User){
 
+  user:User; // the underlying user object, a javascript implementation of inheritance
+	
+  constructor(user:User)
+	{
+		this.user = user;
   }
 
 
 }
 
 export class GuestUser {
-  user:User;
-  constructor(user:User){
-
+  
+	user:User; // the underlying user object, a javascript implementation of inheritance
+	
+  constructor(user:User)
+	{
+		this.user = user;
   }
 
-  registerUser(){
+	// 
+  registerUser()
+	{
 
   }
 }
