e0261eaa05a67598daa06bbafae38584a30c8185
Gareth <garethjsharpe@gmail.com>
2017-07-21 13:23:48 -0400

    Search autocomplete for Maps

--- src/app/amg-map/amg-map.component.ts
+++ src/app/amg-map/amg-map.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
+import { ElementRef, NgZone, ViewChild, Component, OnInit } from '@angular/core';
 import { WebAPI } from '../web-api.service';
 import { Beacon } from '../beacon';
+import { MapsAPILoader } from '@agm/core';
+import {} from '@types/googlemaps';
+
+import { FormControl } from '@angular/forms';
 
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
@@ -16,92 +19,133 @@ import * as firebase from 'firebase/app';
 
 export class AmgMapComponent implements OnInit {
 
-	database = firebase.database();
-	beacons:any[] = [];
-	user: Observable<firebase.User>;
-  items: FirebaseListObservable<any[]>;
-  position:any = "";
+	private database = firebase.database();
+	private beacons:any[] = [];
+	private user: Observable<firebase.User>;
+  private items: FirebaseListObservable<any[]>;
+  private position: any;
+  public searchControl: FormControl;
 
 	// google maps zoom level
-	zoom: number = 6;
-
+	private zoom: number = 6;
   // initial center position for the map
-  lat: number = 0;
-  lng: number = 0;
+  private lat: number = 0;
+  private lng: number = 0;
 
-	constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase,  private webAPI:WebAPI) { }
+  @ViewChild("search")
+  public searchElementRef: ElementRef;
 
-      ngOnInit() {
+	constructor(
+    private afAuth: AngularFireAuth, 
+    private af: AngularFireDatabase,  
+    private webAPI:WebAPI,
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone) { }
 
-      //create a random new Beacon for testing purposes
-      this.createBeacon("CP317", "Laurier",
-          new Date().getTime(),
-          new Date().getTime() + 1 +  Math.floor(Math.random() * 21600000),
-          "123456789ABCDEFG",
-          43.4724 + (Math.random()-0.5),
-          -80.526 + (Math.random()-0.5),
-          "0010");
-      // gets beacons from firebase
-      this.getBeacons();
-      // sets initial map position based on user location
-      this.getPosition();
-    }
+  ngOnInit() {
 
-    // display all beacons on the screen
-    getBeacons() {
-        this.webAPI.getBeacons().then(res => {
-        for (var key in res.val()) {
-          this.beacons.push(res.val()[key]);
-        }
-        //console.log(this.beacons);
+    //create a random new Beacon for testing purposes
+    this.createBeacon("CP317", "Laurier",
+        new Date().getTime(),
+        new Date().getTime() + 1 +  Math.floor(Math.random() * 21600000),
+        "123456789ABCDEFG",
+        43.4724 + (Math.random()-0.5),
+        -80.526 + (Math.random()-0.5),
+        "0010");
+    // gets beacons from firebase
+    this.getBeacons();
+    // sets initial map position based on user location
+    this.getPosition();
+    // creates serach FromControl
+    this.searchControl = new FormControl()
+
+    //load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ["address"]
       });
-    }
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
 
-    // create a new Beacon, which is first instantiated as a class and then stored in the database
-    createBeacon(course:string, school:string, startTime:number, endTime:number, host:string, lat:number, lng:number, tags:string)
-    {
-      var b = new Beacon({
-        school: school,
-        course: course,
-        startTime: startTime,
-        endTime: endTime,
-        host: host,
-        lat: lat,
-        lng: lng,
-        tags: tags});
-      console.log(b);
-      b.storeBeacon();
-    }
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
 
-    // gets the position of user from their browser and calls setPostion()
-    getPosition() {
-      if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(this.setPosition.bind(this));
+          //set latitude, longitude and zoom
+          this.lat = place.geometry.location.lat();
+          this.lng = place.geometry.location.lng();
+          this.zoom = 12;
+        });
+      });
+    });
+  }
+
+  // display all beacons on the screen
+  getBeacons() {
+      this.webAPI.getBeacons().then(res => {
+      for (var key in res.val()) {
+        this.beacons.push(res.val()[key]);
       }
-    }
+      //console.log(this.beacons);
+    });
+  }
 
-    // sets the maps lat and lng attributes given a position
-    setPosition(position) {
-      this.lat = position.coords.latitude;
-      this.lng = position.coords.longitude;
-      //console.log(position.coords);
-    }
+  // create a new Beacon, which is first instantiated as a class and then stored in the database
+  createBeacon(
+    course:string, 
+    school:string, 
+    startTime:number, 
+    endTime:number, 
+    host:string, 
+    lat:number, 
+    lng:number, 
+    tags:string)
+  {
+    var b = new Beacon({
+      school: school,
+      course: course,
+      startTime: startTime,
+      endTime: endTime,
+      host: host,
+      lat: lat,
+      lng: lng,
+      tags: tags});
+    console.log(b);
+    b.storeBeacon();
+  }
 
-    // returns the maps current center latitude and longitude
-    getMapCenter() {
-      //console.log(this.lat);
-      //console.log(this.lng);
-      return this.lat, this.lng;
+  // gets the position of user from their browser and calls setPostion()
+  getPosition() {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.setPosition.bind(this));
     }
+  }
 
-    // logs when the map has loaded
-    mapReady($event: any) {
-      console.log("Ready");
-    }
+  // sets the maps lat and lng attributes given a position
+  setPosition(position) {
+    this.lat = position.coords.latitude;
+    this.lng = position.coords.longitude;
+    //console.log(position.coords);
+  }
 
-    // places a beacon given an amg-map mapClick $event
-    placeBeacon($event: any) {
-      console.log($event.coords.lat);
-      console.log($event.coords.lng);
+  // returns the maps current center latitude and longitude
+  getMapCenter() {
+    //console.log(this.lat);
+    //console.log(this.lng);
+    return this.lat, this.lng;
+  }
+
+  // logs when the map has loaded
+  mapReady($event: any) {
+    console.log("Ready");
+  }
+
+  // places a beacon given an amg-map mapClick $event
+  placeBeacon($event: any) {
+    console.log($event.coords.lat);
+    console.log($event.coords.lng);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AgmCoreModule } from '@agm/core';
 import { AngularFireModule } from 'angularfire2';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 // New imports to update based on AngularFire2 version 4
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
@@ -23,16 +24,19 @@ export const firebaseConfig = {
 @NgModule({
   declarations: [
     AppComponent,
-    AmgMapComponent
+    AmgMapComponent,
   ],
   imports: [
     BrowserModule,
     AgmCoreModule.forRoot({
-      apiKey:'AIzaSyC6ySrInpH3svhfDbZDIWc3dhHAOvZW2kk'
+      apiKey:'AIzaSyC6ySrInpH3svhfDbZDIWc3dhHAOvZW2kk',
+      libraries: ["places"]
     }),
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [WebAPI],
   bootstrap: [AppComponent]
