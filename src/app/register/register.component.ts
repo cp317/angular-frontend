f8995f3c3103b21cc542947292042c8622e4758d
ethyl-alcohol <ethylchan@yahoo.ca>
2017-07-31 12:51:25 -0400

    updated register.component.ts

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
 })
-export class AppComponent {
-  title = 'pp';
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
 }
