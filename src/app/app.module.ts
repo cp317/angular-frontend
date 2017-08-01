84b2292bad4f63382477185407bb9c835e255561
yaaver2468 <yaaver_imran@hotmail.com>
2017-08-01 17:50:38 -0400

    pushed in user discovery and optmized beacon page
    the user cards are currently hard coded templates, possibly going to be
    made into a separate component, also temporarily adjusted beacon page so
    that it doesn't take up several pages worth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { WebAPI } from './web-api.service';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { UserDiscoveryComponent } from './user-discovery/user-discovery.component';
 import { BeaconCardsComponent } from './beacon-cards/beacon-cards.component';
 import { BeaconSearchComponent } from './beacon-search/beacon-search.component';
 import { BeaconListComponent } from './beacon-list/beacon-list.component';
@@ -38,6 +39,7 @@ export const firebaseConfig = {
     AppComponent,
     MapComponent,
     NavbarComponent,
+    UserDiscoveryComponent,
     BeaconCardsComponent,
     BeaconSearchComponent,
     BeaconListComponent,
---
+++ src/app/user-discovery/user-discovery.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-discovery',
+  templateUrl: './user-discovery.component.html',
+  styleUrls: ['./user-discovery.component.css','./user-cards.component.css']
+})
+export class UserDiscoveryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
