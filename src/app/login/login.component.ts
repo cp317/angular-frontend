da6152a4235c4e43273843702c64488690106450
garrettparris <garrettp18@gmail.com>
2017-08-03 14:05:23 -0400

    tweaked guest login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -53,15 +53,5 @@ export class LoginComponent implements OnInit {
             console.log(error.message);;
         
             });
-        firebase.auth().onAuthStateChanged(function(user) {
-            if (user) {
-            // User is signed in.
-            var isAnonymous = user.isAnonymous;
-            var uid = user.uid;
-            // ...
-            } else {
-            // User is signed out.
-            }
-    });
 }
 }
