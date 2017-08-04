b53a6fdcf7a3f1b2578f3854cc7242ebe7412548
Bbogusat <benbogusat@gmail.com>
2017-08-03 20:14:28 -0400

    Base register user and register anon functions

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-register',
@@ -11,4 +12,31 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  attemptRegistration(){
+      var email = (<HTMLInputElement>document.getElementById("inputEmail3")).value;
+      console.log(email)
+      var password = (<HTMLInputElement>document.getElementById("inputPassword3")).value;
+      console.log(password)
+      this.registerUser(email,password)
+  }
+
+  registerUser(email:string, password:string){
+      firebase.auth().createUserWithEmailAndPassword(email, password).catch(
+        function(error){
+          var errorCode = error.stack;
+          var errorMessage = error.message
+          console.log(error.message)
+        });
+  }
+
+  registerAnonUser(email:string, password:string){
+    var credential = firebase.auth.EmailAuthProvider.credential(email, password)
+    firebase.auth().currentUser.linkWithCredential(credential).then(function(user) {
+      console.log("Anonymous account successfully upgraded", user);
+    }, function(error) {
+        console.log("Error upgrading anonymous account", error.message);
+    });
+  }
+
 }
