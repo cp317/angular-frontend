84f52753b581a96ddfd62b21453223e2db142d8a
Wenbo-Han <hanx1980@mylaurier.ca>
2017-08-06 23:17:03 -0400

    import the wrong path

--- src/app/map/map.component.spec.ts
+++ src/app/map/map.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MapComponent } from './amg-map.component';
+import { MapComponent } from './map.component';
 
 describe('MapComponent', () => {
   let component: MapComponent;
