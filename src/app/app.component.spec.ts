f73743aed2acdd7fa9fd8a642e0e1d74e70696f8
Wenbo-Han <hanx1980@mylaurier.ca>
2017-08-09 18:00:19 -0400

    unit testing for checkEmail function

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,32 +1,34 @@
 import { TestBed, async } from '@angular/core/testing';
 
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component : AppComponent
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
 
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
-  }));
+ // checkEmail(email:string,callback)
+
+  it('should return false if email does not already exists', async() => {
+      component.checkEmail('afasfsafasf',function(a){
+      expect(a).toBeFalsy();
+    }); 
+  })
+
+
+  it('should return true if email does not already exists', async() => {
+      component.checkEmail('MattMurdock96@gmail.com',function(a){
+      expect(a).toBeTruthy();
+    });    
+  })
+
+
 });
